f072719ecba4f4e7b4c8f9ae152ac745f441260f
David Pinney <david@pinney.org>
2020-12-04 14:34:45 -0500

    entrypoint spec

---
+++ entrypoints.py
@@ -0,0 +1,14 @@
+# print('test')
+
+def nerf(scene_folder):
+	# code goes here
+	# probably want to have documentation on what a valid scene_folder is
+	return '3d model? video? many things?'
+
+def pole_classifier_train(training_images_folder):
+	# code goes here
+	return 'trained model'
+
+def pole_classifier_classify(path_to_image_file, model=None):
+	# code goes here
+	return 'classification score(s)'
