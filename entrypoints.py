88a48df7c31a7330426f3c11b203a97e7f7a3bd0
David Pinney <david@pinney.org>
2020-12-11 17:24:06 -0500

    syntax fixes

--- entrypoints.py
+++ entrypoints.py
@@ -15,13 +15,17 @@ def pole_classifier_classify(path_to_image_file, model=None):
 	# code goes here
 	return 'classification score(s)'
 
-def llff_poses(scenedir)
-	os.system(python LLFF/imgs2poses.py scenedir)
+def llff_poses(scenedir_path):
+	os.system('python2 LLFF/imgs2poses.py ' + scenedir_path)
 	return "scenedir/poses_bounds.npy"
 
-def llff_spiral_render(llff_poses)
+def llff_spiral_render(llff_poses):
 	llff_poses(scenedir) 
-	os.system(python imgs2mpis.py scenedir scenedir/mpis --height 360)
-	os.system(python imgs2renderpath.py scenedir scenedir/spiral_path.txt --spiral)
-	os.system(python mpis2video.py scenedir/mpis scenedir/spiral_path.txt scenedir/spiral_render.mp4 --crop_factor 0.8)
+	os.system('python2 imgs2mpis.py scenedir scenedir/mpis --height 360')
+	os.system('python2 imgs2renderpath.py scenedir scenedir/spiral_path.txt --spiral')
+	os.system('python2 mpis2video.py scenedir/mpis scenedir/spiral_path.txt scenedir/spiral_render.mp4 --crop_factor 0.8')
 	return 'LLFF/scenedir/spiral_render.mp4'
+
+llff_poses('./LLFF/scenedir')
+
+# llff_spiral_render()
