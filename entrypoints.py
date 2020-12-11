d535d8d8ffcb51120f4a49b35d99bbbe4e49ed02
Thomas <tjankovic12@gmail.com>
2020-12-11 13:46:48 -0500

    adds entrypoint for LLFF

--- entrypoints.py
+++ entrypoints.py
@@ -1,3 +1,5 @@
+import os 
+
 # print('test')
 
 def nerf(scene_folder):
@@ -12,3 +14,10 @@ def pole_classifier_train(training_images_folder):
 def pole_classifier_classify(path_to_image_file, model=None):
 	# code goes here
 	return 'classification score(s)'
+
+def llff_poses(scenedir)
+	os.system(python LLFF/imgs2poses.py scenedir)
+	os.system(python imgs2mpis.py scenedir scenedir/mpis --height 360)
+	os.system(python imgs2renderpath.py scenedir scenedir/spiral_path.txt --spiral)
+	os.system(python mpis2video.py scenedir/mpis scenedir/spiral_path.txt scenedir/spiral_render.mp4 --crop_factor 0.8)
+	return 'LLFF/scenedir/spiral_render.mp4'
