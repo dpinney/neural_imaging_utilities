92de46057bf3b8d6a65c48b2aa0452ae0faee96f
Thomas <tjankovic12@gmail.com>
2020-12-11 16:59:17 -0500

    commits for meeting

--- entrypoints.py
+++ entrypoints.py
@@ -3,7 +3,7 @@ import os
 # print('test')
 
 def nerf(scene_folder):
-	# code goes here
+	poses = llff_poses(scenedir)
 	# probably want to have documentation on what a valid scene_folder is
 	return '3d model? video? many things?'
 
@@ -17,6 +17,10 @@ def pole_classifier_classify(path_to_image_file, model=None):
 
 def llff_poses(scenedir)
 	os.system(python LLFF/imgs2poses.py scenedir)
+	return "scenedir/poses_bounds.npy"
+
+def llff_spiral_render(llff_poses)
+	llff_poses(scenedir) 
 	os.system(python imgs2mpis.py scenedir scenedir/mpis --height 360)
 	os.system(python imgs2renderpath.py scenedir scenedir/spiral_path.txt --spiral)
 	os.system(python mpis2video.py scenedir/mpis scenedir/spiral_path.txt scenedir/spiral_render.mp4 --crop_factor 0.8)
