044bdcd1398b736299eab7e18a00f85b9a5c4080
Thomas <tjankovic12@gmail.com>
2021-01-06 18:31:36 -0500

    accounts for os.system creates new process and terminates it at the end

--- entrypoints.py
+++ entrypoints.py
@@ -13,34 +13,29 @@ def nerf():
 	# 2. Duplicate and rename nerf/config_fern.txt
 	# 3. Within the above renamed file, rename expname and datadir
 	os.system('python run_nerf.py --config nerf/config_fern.txt')
-	os.system('deactivate')
 	return 'object geometry and visual representation'
 
 def pole_classifier_train(training_images_folder):
 	os.system('. TF_Transfer_Learning/venv-TFTL/bin/activate')
 	transfer_learning(training_images_folder)
-	os.system('deactivate')
 	return 'trained model'
 
 def pole_classifier_classify(path_to_image_file, model=None):
 	os.system('. TF_Transfer_Learning/venv-TFTL/bin/activate')
 	image_classifier(path_to_image_file)
-	os.system('deactivate')
 	return 'classification score(s)'
 
 def llff_poses(scenedir_path):
-	os.system('. LLFF/venv-LLFF/bin/activate')
-	os.system('python2 LLFF/imgs2poses.py ' + scenedir_path)
-	os.system('deactivate')
+	os.system('. LLFF/venv-LLFF/bin/activate && python2 LLFF/imgs2poses.py ' + scenedir_path)
+	# os.system('python2 LLFF/imgs2poses.py ' + scenedir_path)
 	return 'scenedir/poses_bounds.npy'
 
 def llff_spiral_render():
 	llff_poses('./LLFF/scenedir')
-	os.system('. LLFF/venv-LLFF/bin/activate') 
-	os.system('python2 LLFF/imgs2mpis.py LLFF/scenedir LLFF/scenedir/mpis --height 360')
-	os.system('python2 LLFF/imgs2renderpath.py LLFF/scenedir LLFF/scenedir/spiral_path.txt --spiral')
-	os.system('python2 LLFF/mpis2video.py LLFF/scenedir/mpis LLFF/scenedir/spiral_path.txt LLFF/scenedir/spiral_render.mp4 --crop_factor 0.8')
-	os.system('deactivate')
+	os.system('. LLFF/venv-LLFF/bin/activate && python2 LLFF/imgs2mpis.py LLFF/scenedir LLFF/scenedir/mpis --height 360 && python2 LLFF/imgs2renderpath.py LLFF/scenedir LLFF/scenedir/spiral_path.txt --spiral && python2 LLFF/mpis2video.py LLFF/scenedir/mpis LLFF/scenedir/spiral_path.txt LLFF/scenedir/spiral_render.mp4 --crop_factor 0.8') 
+	# os.system('python2 LLFF/imgs2mpis.py LLFF/scenedir LLFF/scenedir/mpis --height 360')
+	# os.system('python2 LLFF/imgs2renderpath.py LLFF/scenedir LLFF/scenedir/spiral_path.txt --spiral')
+	# os.system('python2 LLFF/mpis2video.py LLFF/scenedir/mpis LLFF/scenedir/spiral_path.txt LLFF/scenedir/spiral_render.mp4 --crop_factor 0.8')
 	return 'LLFF/scenedir/spiral_render.mp4'
 
 # install()
